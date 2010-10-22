2492f64a0b2cf2f28f244e96f7855cc76ef8db3b
sylvanaar <sylvanaar@localhost>
2010-10-22 11:45:54 -0400

    annotator and parser tweaks

---
+++ src/lang/psi/impl/expressions/LuaExpressionImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.impl.expressions;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiType;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaExpression;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiElementImpl;
+import com.sylvanaar.idea.Lua.lang.psi.util.LuaPsiUtils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: Jun 12, 2010
+ * Time: 11:38:37 PM
+ */
+public class LuaExpressionImpl extends LuaPsiElementImpl implements LuaExpression {
+    public LuaExpressionImpl(ASTNode node) {
+        super(node);
+    }
+
+    public String toString() {
+        return "Expr: " + getText().substring(0, Math.min(getText().length(), 10));
+    }
+
+    public PsiElement getContext() {
+        return getParent();
+    }
+
+//    @Override
+//    public PsiType getType() {
+//        return PsiType.VOID;
+//    }
+
+    @Override
+    public PsiElement replaceWithExpression(LuaExpression newExpr, boolean removeUnnecessaryParentheses) {
+        return LuaPsiUtils.replaceElement(this, newExpr);
+    }
+
+    @Override
+    public LuaPsiType getType() {
+        return null;
+    }
+}
