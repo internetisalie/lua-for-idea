c08b08c74e71d27d3475dc04c33729da932b6da1
SYSTEM <SYSTEM@JUPITER.nc.rr.com>
2010-11-27 19:53:39 -0500

    Added tag 0.9.8 for changeset b66547d9cefa

---
+++ src/lang/psi/impl/expressions/LuaLiteralExpressionImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.impl.expressions;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElementVisitor;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaLiteralExpression;
+import com.sylvanaar.idea.Lua.lang.psi.visitor.LuaElementVisitor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: Jun 13, 2010
+ * Time: 12:11:12 AM
+ */
+public class LuaLiteralExpressionImpl extends LuaExpressionImpl implements LuaLiteralExpression {
+    public LuaLiteralExpressionImpl(ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    public String toString() {
+        return "Literal:" + getText() ;
+    }
+
+    @Override
+    public Object getValue() {
+        return getNode().getText();
+    }
+
+    @Override
+    public void accept(LuaElementVisitor visitor) {
+        super.accept(visitor);
+        visitor.visitLiteralExpression(this);
+    }
+
+    @Override
+    public void accept(@NotNull PsiElementVisitor visitor) {
+        super.accept(visitor);
+
+        if (visitor instanceof LuaElementVisitor) {
+            ((LuaElementVisitor) visitor).visitLiteralExpression(this);
+        } else {
+            visitor.visitElement(this);
+        }
+    }
+}
---
+++ src/lang/psi/visitor/LuaElementVisitor.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.visitor;
+
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.psi.PsiElementVisitor;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiElement;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.*;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiKeywordImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiTokenImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.statements.LuaFunctionCallStatementImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.statements.LuaRepeatStatementImpl;
+import com.sylvanaar.idea.Lua.lang.psi.statements.*;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: Jun 12, 2010
+ * Time: 7:39:03 AM
+ */
+
+public class LuaElementVisitor extends PsiElementVisitor {
+    public void visitElement(LuaPsiElement element)
+    {
+        ProgressManager.checkCanceled();
+    }
+
+    public void visitFile(LuaPsiFile e) {
+        visitElement(e);
+    }
+
+    public void visitFunctionDef(LuaFunctionDefinitionStatement e) {
+        visitElement(e);
+    }
+
+    public void visitAssignment(LuaAssignmentStatement e) {
+        visitElement(e);
+    }
+
+    public void visitIdentifier(LuaIdentifier e) {
+        visitElement(e);
+    }
+
+    public void visitStatement(LuaStatementElement e) {
+        visitElement(e);
+    }
+
+    public void visitNumericForStatement(LuaNumericForStatement e) {
+        visitElement(e);
+    }
+
+    public void visitBlock(LuaBlock e) {
+        visitElement(e);
+    }
+
+    public void visitGenericForStatement(LuaGenericForStatement e) {
+        visitElement(e);
+    }
+
+     public void visitIfThenStatement(@NotNull LuaIfThenStatement e) {
+        visitElement(e);
+    }
+
+    public void visitWhileStatement(LuaWhileStatement e) {
+        visitElement(e);
+    }
+
+    public void visitParameter(LuaParameter e) {
+        visitElement(e);
+    }
+
+    public void visitReturnStatement(LuaReturnStatement e) {
+        visitElement(e);
+    }
+
+    public void visitReferenceExpression(LuaReferenceExpression e) {
+        visitElement(e);        
+    }
+
+    public void visitKeyword(LuaPsiKeywordImpl e) {
+        visitElement(e);
+    }
+
+    public void visitLuaToken(LuaPsiTokenImpl e) {
+        visitElement(e);
+    }
+
+    public void visitDeclarationStatement(LuaDeclarationStatement e) {
+        visitElement(e);
+    }
+
+    public void visitDeclarationExpression(LuaDeclarationExpression e) {
+        visitElement(e);
+    }
+
+    public void visitLiteralExpression(LuaLiteralExpression e) {
+        visitElement(e);
+    }
+
+    public void visitTableConstructor(LuaTableConstructor e) {
+        visitElement(e);
+    }
+
+    public void visitUnaryExpression(LuaUnaryExpression e) {
+        visitElement(e);
+    }
+
+    public void visitBinaryExpression(LuaBinaryExpression e) {
+        visitElement(e);
+    }
+
+    public void visitFunctionCall(LuaFunctionCallExpression e) {
+        visitElement(e);
+    }
+
+    public void visitBreakStatement(LuaBreakStatement e) {
+          visitElement(e);
+    }
+
+    public void visitRepeatStatement(LuaRepeatStatementImpl e) {
+        visitElement(e);
+    }
+
+    public void visitFunctionCallStatement(LuaFunctionCallStatementImpl e) {
+        visitElement(e);
+    }
+}
+
+
+
+
