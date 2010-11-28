8742c29a44a9594d65aa85f4ce00e0b3e7e6c701
sylvanaar <sylvanaar@localhost>
2010-11-27 19:18:55 -0500

    fix (possibly) the multiple inspection results for a single item.

---
+++ src/lang/parser/LuaPsiCreator.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.parser;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.sylvanaar.idea.Lua.lang.lexer.LuaElementType;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaDeclarationImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiElementImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiKeywordImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.LuaPsiTokenImpl;
+import com.sylvanaar.idea.Lua.lang.psi.impl.expressions.*;
+import com.sylvanaar.idea.Lua.lang.psi.impl.statements.*;
+
+import static com.sylvanaar.idea.Lua.lang.parser.LuaElementTypes.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: Apr 14, 2010
+ * Time: 6:56:50 PM
+ */
+public class LuaPsiCreator {
+
+    public static PsiElement createElement(ASTNode node) {
+        IElementType elem = node.getElementType();
+
+        if (elem instanceof LuaElementType.PsiCreator) {
+            return ((LuaElementType.PsiCreator) elem).createPsi(node);
+        }
+
+        if (node.getElementType() == TOKEN_OR_KEYWORD) {
+            if (KEYWORDS.contains(node.getElementType()))
+                return new LuaPsiKeywordImpl(node.getElementType(), node.getChars());
+
+            return new LuaPsiTokenImpl(node.getElementType(), node.getChars());
+        }
+
+        if (node.getElementType() == EXPR)
+            return new LuaExpressionImpl(node);
+
+        if (node.getElementType() == FUNCTION_CALL_EXPR)
+            return new LuaFunctionCallExpressionImpl(node);
+
+        if (node.getElementType() == ANONYMOUS_FUNCTION_EXPRESSION)
+            return new LuaAnonymousFunctionExpressionImpl(node);
+
+        if (node.getElementType() == CONDITIONAL_EXPR)
+            return new LuaConditionalExpressionImpl(node);
+
+        if (node.getElementType() == REFERENCE)
+            return new LuaReferenceExpressionImpl(node);
+
+        if (node.getElementType() == TABLE_CONSTUCTOR)
+            return new LuaTableConstructorImpl(node);
+
+        if (node.getElementType() == IDX_ASSIGNMENT)
+            return new LuaExpressionImpl(node);        
+        if (node.getElementType() == KEY_ASSIGNMENT)
+            return new LuaKeyValueInitializerImpl(node);
+
+        if (node.getElementType() == BLOCK)
+            return new LuaBlockImpl(node);
+        if (node.getElementType() == REPEAT_BLOCK)
+            return new LuaRepeatStatementImpl(node);
+        
+        if (node.getElementType() == LOCAL_DECL)
+            return new LuaLocalDefinitionStatementImpl(node);
+
+        if (node.getElementType() == LOCAL_DECL_WITH_ASSIGNMENT)
+            return new LuaLocalDefinitionStatementImpl(node);
+
+        if (node.getElementType() == EXPR_LIST)
+            return new LuaExpressionListImpl(node);
+
+        if (node.getElementType() == IDENTIFIER_LIST)
+            return new LuaIdentifierListImpl(node);
+
+        if (node.getElementType() == VARIABLE)
+            return new LuaVariableImpl(node);
+        
+        if (node.getElementType() == LITERAL_EXPRESSION)
+            return new LuaLiteralExpressionImpl(node);
+
+        if (node.getElementType() == BINARY_EXP)
+            return new LuaBinaryExpressionImpl(node);
+        if (node.getElementType() == UNARY_EXP)
+            return new LuaUnaryExpressionImpl(node);
+
+        if (node.getElementType() == FUNCTION_CALL)
+            return new LuaFunctionCallStatementImpl(node);
+
+        if (node.getElementType() == RETURN_STATEMENT ||
+                node.getElementType() == RETURN_STATEMENT_WITH_TAIL_CALL)
+            return new LuaReturnStatementImpl(node);
+
+        if (node.getElementType() == NUMERIC_FOR_BLOCK)
+            return new LuaNumericForStatementImpl(node);
+
+        if (node.getElementType() == GENERIC_FOR_BLOCK)
+            return new LuaGenericForStatementImpl(node);
+
+        if (node.getElementType() == WHILE_BLOCK)
+            return new LuaWhileStatementImpl(node);
+
+        if (node.getElementType() == ASSIGN_STMT)
+            return new LuaAssignmentStatementImpl(node);
+
+        if (node.getElementType() == DO_BLOCK)
+            return new LuaDoStatementImpl(node);
+
+        if (node.getElementType() == IF_THEN_BLOCK)
+            return new LuaIfThenStatementImpl(node);
+
+        if (node.getElementType() == SELF_PARAMETER)
+            return new LuaImpliedSelfParameterImpl(node);
+
+        if (node.getElementType() == FUNCTION_IDENTIFIER)
+            return new LuaFunctionIdentifierDefImpl(node);
+
+        if (node.getElementType() == GLOBAL_NAME)
+            return new LuaIdentifierImpl(node);
+
+        if (node.getElementType() == LOCAL_NAME_DECL)
+            return new LuaDeclarationImpl(node);
+
+        if (node.getElementType() == LOCAL_NAME)
+            return new LuaIdentifierImpl(node);
+
+        if (node.getElementType() == FIELD_NAME)
+            return new LuaIdentifierImpl(node);
+
+        if (node.getElementType() == FUNCTION_DEFINITION )
+            return new LuaFunctionDefinitionStatementImpl(node);
+        if (node.getElementType() == LOCAL_FUNCTION)
+            return new LuaLocalFunctionDefinitionStatementImpl(node);
+        if (node.getElementType() == LuaElementTypes.PARAMETER_LIST)
+            return new LuaParameterListImpl(node);
+
+        if (node.getElementType() == LuaElementTypes.PARAMETER)
+            return new LuaParameterImpl(node);
+
+        
+        return new LuaPsiElementImpl(node);
+    }
+
+}
