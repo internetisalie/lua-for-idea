a8be1894f67a96aa9317a13acff7a8a4386c549b
sylvanaar <sylvanaar@localhost>
2010-12-23 12:08:32 -0500

    fix lexing of hexadecimal numbers

---
+++ src/editor/completion/LuaCompletionContributor.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.editor.completion;
+
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaIdentifier;
+import com.sylvanaar.idea.Lua.lang.psi.visitor.LuaRecursiveElementVisitor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+
+public class LuaCompletionContributor extends DefaultCompletionContributor {
+    private static final Logger log = Logger.getInstance("#Lua.CompletionContributor");
+
+    private static final ElementPattern<PsiElement> AFTER_DOT = psiElement().afterLeaf(".", ":").withParent(LuaIdentifier.class);
+    private static final ElementPattern<PsiElement> NOT_AFTER_DOT = psiElement().andNot(psiElement().afterLeaf(".", ":"));//.withParent(LuaVariable.class);
+
+    public LuaCompletionContributor() {
+        log.info("Created Lua completion contributor");
+
+        extend(CompletionType.BASIC, NOT_AFTER_DOT, new CompletionProvider<CompletionParameters>() {
+            @Override
+            protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
+                for (String s : LuaKeywordsManager.getKeywords())
+                    result.addElement(new LuaLookupElement(s));
+            }
+        });
+
+        extend(CompletionType.BASIC, AFTER_DOT, new CompletionProvider<CompletionParameters>() {
+            @Override
+            protected void addCompletions(@NotNull CompletionParameters parameters,
+                                          ProcessingContext context,
+                                          @NotNull CompletionResultSet result) {
+
+                log.info("add field completions");
+
+                fieldVisitor.reset();
+
+                ((LuaPsiFile)parameters.getOriginalFile()).accept(fieldVisitor);
+
+                for (String s : fieldVisitor.getResult()) {
+                    log.info(s);
+                    result.addElement(new LuaLookupElement(s));
+                }
+            }
+        });
+
+    }
+
+    LuaFieldElementVisitor fieldVisitor = new LuaFieldElementVisitor();
+
+    private static class LuaFieldElementVisitor extends LuaRecursiveElementVisitor {
+        Set<String> result = new HashSet<String>();
+
+        @Override
+        public void visitIdentifier(LuaIdentifier e) {
+            super.visitIdentifier(e);
+
+            if (e.isField() && e.getText().charAt(0) != '[')
+                result.add(e.getName());
+
+        }
+
+        public Set<String> getResult() {
+            return result;
+        }
+
+        public void reset() { result.clear(); }
+    }
+}
---
+++ src/lang/psi/impl/statements/LuaGenericForStatementImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2010 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.impl.statements;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.ResolveState;
+import com.intellij.psi.scope.PsiScopeProcessor;
+import com.sylvanaar.idea.Lua.lang.psi.LuaPsiFile;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaExpression;
+import com.sylvanaar.idea.Lua.lang.psi.statements.LuaBlock;
+import com.sylvanaar.idea.Lua.lang.psi.statements.LuaGenericForStatement;
+import com.sylvanaar.idea.Lua.lang.psi.visitor.LuaElementVisitor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: Sep 13, 2010
+ * Time: 2:12:45 AM
+ */
+public class LuaGenericForStatementImpl extends LuaStatementElementImpl implements LuaGenericForStatement {
+    public LuaGenericForStatementImpl(ASTNode node) {
+        super(node);
+    }
+
+
+    public boolean processDeclarations(@NotNull PsiScopeProcessor processor,
+                                       @NotNull ResolveState resolveState,
+                                       PsiElement lastParent,
+                                       @NotNull PsiElement place) {
+
+        PsiElement parent = place.getParent();
+        while(!(parent instanceof LuaPsiFile)) {
+            if (parent == getBody() ) {
+                LuaExpression[] names = getIndices();
+                for (LuaExpression name : names) {
+                     if (!processor.execute(name, resolveState)) return false;
+                }
+            }
+
+            parent = parent.getParent();
+        }
+       return true;
+    }
+
+    @Override
+    public LuaExpression[] getIndices() {
+        LuaExpression[] e = findChildrenByClass(LuaExpression.class);
+
+        e = e.clone();
+        e[e.length-1]=null;
+
+        return e;
+    }
+
+    @Override
+    public LuaExpression getInClause() {
+        LuaExpression[] e = findChildrenByClass(LuaExpression.class);
+
+        return e[e.length-1];
+    }
+
+    @Override
+    public LuaBlock getBody() {
+        return findChildByClass(LuaBlock.class);
+    }
+
+        @Override
+    public void accept(LuaElementVisitor visitor) {
+        visitor.visitGenericForStatement(this);
+    }
+
+    @Override
+    public void accept(@NotNull PsiElementVisitor visitor) {
+        if (visitor instanceof LuaElementVisitor) {
+            ((LuaElementVisitor) visitor).visitGenericForStatement(this);
+        } else {
+            visitor.visitElement(this);
+        }
+    }
+
+}
