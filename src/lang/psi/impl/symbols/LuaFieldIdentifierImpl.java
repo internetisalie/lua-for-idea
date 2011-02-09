c06ac5f6c7ed48a451f54e87fe0a20bd5a8be097
sylvanaar <sylvanaar@localhost>
2011-02-09 11:37:15 -0500

    try stubs again

---
+++ src/lang/psi/impl/symbols/LuaFieldIdentifierImpl.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2011 Jon S Akhtar (Sylvanaar)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.impl.symbols;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiType;
+import com.intellij.util.IncorrectOperationException;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaExpression;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.LuaFieldIdentifier;
+import com.sylvanaar.idea.Lua.lang.psi.symbols.LuaCompoundIdentifier;
+import com.sylvanaar.idea.Lua.lang.psi.symbols.LuaSymbol;
+import com.sylvanaar.idea.Lua.lang.psi.visitor.LuaElementVisitor;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: 1/15/11
+ * Time: 1:31 AM
+ */
+public class LuaFieldIdentifierImpl  extends LuaSymbolImpl implements LuaFieldIdentifier {
+    public LuaFieldIdentifierImpl(ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    public PsiElement setName(@NonNls String name) throws IncorrectOperationException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public PsiElement replaceWithExpression(LuaExpression newCall, boolean b) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+//    @Override
+//    public PsiReference getReference() {
+//        return (PsiReference) getCompositeIdentifier().getEnclosingIdentifier().getParent();
+//    }
+
+    @Override
+    public boolean isSameKind(LuaSymbol identifier) {
+        return identifier instanceof LuaFieldIdentifier;
+    }
+
+    @Override
+    public void accept(LuaElementVisitor visitor) {
+        visitor.visitIdentifier(this);
+    }
+
+    @Override
+    public void accept(@NotNull PsiElementVisitor visitor) {
+        if (visitor instanceof LuaElementVisitor) {
+            ((LuaElementVisitor) visitor).visitIdentifier(this);
+        } else {
+            visitor.visitElement(this);
+        }
+    }
+
+    public boolean  isDeclaration() {
+        return isAssignedTo();
+    }
+
+    @Override
+    public boolean isAssignedTo() {
+        LuaCompoundIdentifier v = getCompositeIdentifier();
+
+        if (v == null)
+            return true; // the only times fields are not part of a composite identifier are table constructors.
+
+        return false;//v.isAssignedTo();
+    }
+
+    public LuaCompoundIdentifier getCompositeIdentifier() {
+        if (getParent() instanceof LuaCompoundIdentifier)
+            return ((LuaCompoundIdentifier) getParent()).getEnclosingIdentifier();
+
+        return null;
+    }
+    @Override
+    public String toString() {
+        return "Field: " + getText();
+    }
+
+
+
+
+    @Override
+    public PsiType getType() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public LuaCompoundIdentifier getEnclosingIdentifier() {
+        return getCompositeIdentifier();
+    }
+}
---
+++ src/lang/psi/resolve/processors/SymbolResolveProcessor.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2011 Jon S Akhtar (Sylvanaar)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.resolve.processors;
+
+import com.intellij.openapi.util.Key;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveState;
+import com.intellij.psi.scope.NameHint;
+import com.sylvanaar.idea.Lua.lang.psi.LuaReferenceElement;
+import com.sylvanaar.idea.Lua.lang.psi.impl.symbols.LuaCompoundReferenceElementImpl;
+import com.sylvanaar.idea.Lua.lang.psi.resolve.LuaResolveResultImpl;
+import com.sylvanaar.idea.Lua.lang.psi.symbols.LuaGlobal;
+import com.sylvanaar.idea.Lua.lang.psi.symbols.LuaSymbol;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * @author ilyas
+ */
+public class SymbolResolveProcessor extends ResolveProcessor implements NameHint {
+
+  private final Set<PsiElement> myProcessedElements = new HashSet<PsiElement>();
+  private final PsiElement myPlace;
+  private final boolean incompleteCode;
+
+
+  public SymbolResolveProcessor(String myName, PsiElement myPlace, boolean incompleteCode) {
+    super(myName);
+    this.myPlace = myPlace;
+    this.incompleteCode = incompleteCode;
+
+  }
+
+    public boolean isFilter() {
+        return filter;
+    }
+
+    public void setFilter(boolean filter) {
+        this.filter = filter;
+    }
+
+    private boolean filter = true;
+
+
+
+
+  public boolean execute(PsiElement element, ResolveState resolveState) {
+  
+    if (element instanceof LuaSymbol && !myProcessedElements.contains(element)) {
+      LuaSymbol namedElement = (LuaSymbol) element;
+      boolean isAccessible = isAccessible(namedElement);
+      if (!filter || isAccessible)
+          myCandidates.add(new LuaResolveResultImpl(namedElement, isAccessible));
+      myProcessedElements.add(namedElement);
+      return !filter || !isAccessible || ((LuaReferenceElement)myPlace).getElement() instanceof LuaGlobal;
+    }
+
+    return true;
+  }
+
+  /*
+  todo: add ElementClassHints
+   */
+  public <T> T getHint(Key<T> hintKey) {
+    if (hintKey == NameHint.KEY && myName != null) {
+      return (T) this;
+    }
+
+    return null;
+  }
+
+  public PsiElement getPlace() {
+    return myPlace;
+  }
+
+  public String getName(ResolveState resolveState) {
+    return myName;
+  }
+
+  public boolean shouldProcess(DeclaractionKind kind) {
+    return true;
+  }
+
+    protected boolean isAccessible(LuaSymbol namedElement) {
+        if (myName == null) return true;
+
+
+        if (myPlace instanceof LuaCompoundReferenceElementImpl) {
+//            System.out.println(myPlace+":  Checking " + namedElement);
+            return myName.equals(namedElement.getName());
+        } else if (myPlace instanceof LuaReferenceElement) {
+            return myName.equals(namedElement.getName()) && namedElement.isSameKind((LuaSymbol) ((LuaReferenceElement) myPlace).getElement());
+        } else {
+            assert false;
+           //return myName.equals(namedElement.getName()) && namedElement.isSameKind((LuaSymbol) myPlace);
+        }
+        return true;
+    }
+}
