978ad7617efb715bd2f723385a063299e8764a96
Michael Crawford <internetisalie@gmail.com>
2016-01-25 18:08:11 -0500

    issue #172: parsing is slow - Stop forcing debug mode for PsiBuilderImpl at all times - Keep allow debug mode for when debugging is actually enabled

--- src/lang/parser/kahlua/KahluaParser.java
+++ src/lang/parser/kahlua/KahluaParser.java
@@ -1891,7 +1891,7 @@ public class KahluaParser implements PsiParser, LuaElementTypes {
 
             lexstate.checkAmbiguituy = false;
             lexstate.builder = psiBuilder;
-            psiBuilder.debug();
+//            psiBuilder.debug();
             psiBuilder.setWhitespaceSkippedCallback(lexstate.new ParserWhitespaceSkippedCallback());
 
             FuncState funcstate = new FuncState(lexstate);
